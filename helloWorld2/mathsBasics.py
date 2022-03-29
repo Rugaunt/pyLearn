988443b40172e21829575b15a98771e522089492
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-03-29 15:44:09 -0600

    maths syntax tutorial
    quick creation of maths syntax for python, basic operators

---
+++ helloWorld2/mathsBasics.py
@@ -0,0 +1,26 @@
+print('operands')
+x = input('first number: ')
+y = input('second number: ')
+x = float(x)
+y = float(y)
+print('addition')
+first = x + y
+print(first)
+print('subtraction')
+second = x - y
+print(second)
+print('division')
+third = x / y
+print(third)
+print('division (integer)')
+fourth = x // y
+print(fourth)
+print('modulus')
+fifth = x % y
+print(fifth)
+print('multiplication')
+sixth = x * y
+print(sixth)
+print('powers to')
+seventh = x ** y
+print(seventh)
