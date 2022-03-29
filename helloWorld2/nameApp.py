0e7792c27839602e2865a657e1a28871fd3bf9b9
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-03-29 11:35:53 -0600

    Update nameApp.py
    Added string Basics with examples and conditionals

--- helloWorld2/nameApp.py
+++ helloWorld2/nameApp.py
@@ -9,9 +9,24 @@ startPlacer = 0
 msg2 = ''
 msg = f'Your name is {name.capitalize()} right?!?'
 if endPlacer >= 5 :
-    msg2 = "that's a long name"
+    msg2 = "that is a long name"
 else:
     msg2 = 'that is a short name'
 
 print(msg)
 print(msg2)
+
+print('string method basics:')
+print(msg2.upper())
+print(msg2.lower())
+print(msg2.title())
+print(msg2.find('that'))
+print(msg2.replace('that', name))
+if "B" in msg2:
+    print("there's a bee in there!")
+else:
+    print("no bees in here...")
+if "b" in msg:
+    print("but you have a bee!")
+else:
+    print("no bees over there...")
