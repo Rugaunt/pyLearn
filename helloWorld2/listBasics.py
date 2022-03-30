3ee2339622a69dc35c3230b43f5456527017c804
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-03-30 10:57:59 -0600

    added examples
    added examples of list operations

--- helloWorld2/listBasics.py
+++ helloWorld2/listBasics.py
@@ -19,8 +19,46 @@ while johnExists:
         johnExists = False
         # all the Johns have been eliminated
 
+print(names)
+# john returns to the party
+print('a wild john appears!')
+names.append('John')
 
+# and another!
+names.insert(0, 'John')
+print(names)
 
+# how many Johns are there?
+print(names.count(searchName))
 
+# ok ok everybody line up in order!!
+names.sort()
+print(names)
+# let me take attendance
+names2 = names.copy()
+# no no no, the other way!
+names.reverse()
+print(names)
+
+# nah, lets get rid of them
+names.remove('John')
+print(names)
+
+# there are too many people at the party, lets get rid of the last one
+names.append('Vanessa')
+names.pop()
+print(names)
+
+# i still think there is a john at the party... where is he?
+whereIsHe = names.index(searchName)
+print(whereIsHe)
+# is there a Jon at the party
+isJon = ('Jon' in names)
+print(isJon)
+# but is there a John here?
+isJohn = searchName in names
+print(isJohn)
+# actually lets cancel the whole party
+names.clear()
 print(names)
 # modName =
