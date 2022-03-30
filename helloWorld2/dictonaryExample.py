1fb6a3d4e87a232ab180c0e9a9e4d0814d3a3797
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-03-30 11:25:39 -0600

    Dictionary example
    simple example of dictionary use

---
+++ helloWorld2/dictonaryExample.py
@@ -0,0 +1,29 @@
+"""
+Name: John Smith
+Email: erfuh@vrij.cot
+Phone: 111-444-2211
+"""
+
+customer = {
+    "namE": "John Smith",
+    "age": 30,
+    "is_verified": False
+}
+
+print(customer["namE"])
+#                   key      default
+print(customer.get("name", "Unknown"))
+
+numbers = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+}
+
