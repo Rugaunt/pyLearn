a2ae135e76034d0df5636b0c966f9d5b6b06ffbb
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-03-30 11:32:28 -0600

    added number example
    added conversion to long text of numbers

--- helloWorld2/dictonaryExample.py
+++ helloWorld2/dictonaryExample.py
@@ -15,15 +15,22 @@ print(customer["namE"])
 print(customer.get("name", "Unknown"))
 
 numbers = {
-    0: "Zero",
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine"
+    '0': "Zero",
+    '1': "One",
+    '2': "Two",
+    '3': "Three",
+    '4': "Four",
+    '5': "five",
+    '6': "Six",
+    '7': "Seven",
+    '8': "Eight",
+    '9': "Nine"
 }
 
+phone = input("Phone# ")
+# phone = int(phone)
+output = ""
+for ch in phone:
+    output += numbers.get(ch, "!") + ' '
+
+print(output)
