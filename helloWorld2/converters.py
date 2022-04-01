6a7e8a6f283865e9a454caacf4037d2f59e54d35
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-04-01 14:16:54 -0600

    Create converters.py
    conversion from lbs to kg and back

---
+++ helloWorld2/converters.py
@@ -0,0 +1,10 @@
+CONVERSION = 2.2  # simple conversion
+# 2.20462 precise conversion
+
+
+def kgs_to_lbs(weight):
+    return weight * CONVERSION
+
+
+def lbs_to_kgs(weight):
+    return weight / CONVERSION
