78fb4f5a62355655e3743001d9fbdf37528225da
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-04-04 10:22:09 -0600

    finished example
    updates to workbook are working and table generation works

--- helloWorld2/pathingExample.py
+++ helloWorld2/pathingExample.py
@@ -2,9 +2,11 @@ from pathlib import Path
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('tester2.xlsx')
-charSheet = wb['Character']
-historySheet = wb['Tester']
+
+workbook_file_name = 'tester2.xlsx'
+# TODO workbook_file_path = ''
+
+
 
 path = Path()
 print(path.glob('*'))  # all files and directories
@@ -16,37 +18,46 @@ for file in path.glob('*.py'):
     print(file)
 
 
-def update_history(current_hp, x, new_hp):
+def process_workbook(filename):
+    wb = xl.load_workbook(workbook_file_name)
+    char_sheet = wb['Character']
+    history_sheet = wb['Tester']
+    print('losing 5 hitpoints!')
+    change_hitpoints(-5, char_sheet, wb, history_sheet)
+    values = Reference(history_sheet,
+                       min_row=2,
+                       max_row=history_sheet.max_row,
+                       min_col=3,
+                       max_col=3
+                       )
+
+    chart = BarChart()
+    chart.add_data(values)
+    history_sheet.add_chart(chart, 'd4')
+    wb.save(workbook_file_name)
+
+
+def update_history(current_hp, x, new_hp, history_sheet):
     # add to end of sheet for history
-    end_location = historySheet.max_row + 1
-    historySheet.cell(end_location, 1).value = current_hp
-    historySheet.cell(end_location, 2).value = x
-    historySheet.cell(end_location, 3).value = new_hp
+    end_location = history_sheet.max_row + 1
+    history_sheet.cell(end_location, 1).value = current_hp
+    history_sheet.cell(end_location, 2).value = x
+    history_sheet.cell(end_location, 3).value = new_hp
 
 
-def change_hitpoints(x):
-    current_hp = charSheet.cell(8, 1).value
+def change_hitpoints(x, char_sheet, wb, history_sheet):
+    current_hp = char_sheet.cell(8, 1).value
     if x < 0:
         print('losing hit points!')
     new_hp = x + current_hp
-    charSheet.cell(8, 1).value = new_hp
+    char_sheet.cell(8, 1).value = new_hp
     # update history tab
-    update_history(current_hp, x, new_hp)
+    update_history(current_hp, x, new_hp, history_sheet)
     wb.save('tester2.xlsx')
 
 
-print('losing 5 hitpoints!')
-change_hitpoints(-5)
-values = Reference(historySheet,
-                   min_row=2,
-                   max_row=historySheet.max_row,
-                   min_col=3,
-                   max_col=3
-                   )
+process_workbook(workbook_file_name)
 
-chart = BarChart()
-chart.add_data(values)
-historySheet.add_chart(chart, 5, 2)
 
 """
 cell = sheet['a1']  # for some reason its backwards for the next part
