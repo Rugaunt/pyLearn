2425f81c5a4a82ccd315052d8d8cc60aeef320ee
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-04-12 16:15:06 -0600

    Display Update
    checkboxes working. Added item types and min max added

--- ItemGenerator/GeneratorDisplay.py
+++ ItemGenerator/GeneratorDisplay.py
@@ -2,12 +2,34 @@ from tkinter import *
 from tkinter import ttk
 # import dice
 
+START_POS_ROW_MIN_TYPE_ROW = 4
+BASE_POS_MIN_ITEM_COL = 1
+BASE_POS_MAX_ITEM_COL = 3
+BASE_POS_TYPE_ITEM_COL = 5
+
+
+
+
 
 class DiceDisplay:
     def __init__(self, in_root):
-        STARTPOS_ROW_MIN_TYPE_ROW = 4
-        BASE_POS_MIN_ITEM_COL = 1
-        BASE_POS_MAX_ITEM_COL = 3
+        # STARTPOS_ROW_MIN_TYPE_ROW = 4
+        # BASE_POS_MIN_ITEM_COL = 1
+        # BASE_POS_MAX_ITEM_COL = 3
+        # BASE_POS_TYPE_ITEM_COL = 5
+        self.belt = BooleanVar()
+        self.body = BooleanVar()
+        self.chest = BooleanVar()
+        self.eyes = BooleanVar()
+        self.feet = BooleanVar()
+        self.hands = BooleanVar()
+        self.head = BooleanVar()
+        self.headband = BooleanVar()
+        self.neck = BooleanVar()
+        self.shoulders = BooleanVar()
+        self.wrists = BooleanVar()
+        self.slot_less = BooleanVar()
+        self.choose_all_types()
         in_root.title("Magic Item Generator! (Pathfinder 1e)")
 
         mainframe = ttk.Frame(in_root, padding="3 3 12 12")
@@ -25,38 +47,82 @@ class DiceDisplay:
 
         ttk.Label(mainframe, textvariable=self.result).grid(column=2, row=2, sticky=(W, E))
 
-        ttk.Label(mainframe, text="Minimum Item Level").grid(column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW - 1, sticky=W)
+        ttk.Label(mainframe, text="Minimum Item Level").grid(
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW - 1, sticky=W)
         ttk.Button(mainframe, text="Any", command=lambda: self.choose_type_min("Any")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW, sticky=W)
         ttk.Button(mainframe, text="Lesser Minor", command=lambda: self.choose_type_min("Lesser Minor")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 1, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 1, sticky=W)
         ttk.Button(mainframe, text="Greater Minor", command=lambda: self.choose_type_min("Greater Minor")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 2, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 2, sticky=W)
         ttk.Button(mainframe, text="Lesser Medium", command=lambda: self.choose_type_min("Lesser Medium")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 3, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 3, sticky=W)
         ttk.Button(mainframe, text="Greater Medium", command=lambda: self.choose_type_min("Greater Medium")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 4, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 4, sticky=W)
         ttk.Button(mainframe, text="Lesser Major", command=lambda: self.choose_type_min("Lesser Major")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 5, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 5, sticky=W)
         ttk.Button(mainframe, text="Greater Major", command=lambda: self.choose_type_min("Greater Major")).grid(
-            column=BASE_POS_MIN_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 6, sticky=W)
+            column=BASE_POS_MIN_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 6, sticky=W)
 
-        ttk.Label(mainframe, text="Maximum Item Level").grid(column=3, row=STARTPOS_ROW_MIN_TYPE_ROW - 1, sticky=W)
+        ttk.Label(mainframe, text="Maximum Item Level").grid(
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW - 1, sticky=W)
         ttk.Button(mainframe, text="Any", command=lambda: self.choose_type_max("Any")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW, sticky=W)
         ttk.Button(mainframe, text="Lesser Minor", command=lambda: self.choose_type_max("Lesser Minor")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 1, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 1, sticky=W)
         ttk.Button(mainframe, text="Greater Minor", command=lambda: self.choose_type_max("Greater Minor")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 2, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 2, sticky=W)
         ttk.Button(mainframe, text="Lesser Medium", command=lambda: self.choose_type_max("Lesser Medium")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 3, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 3, sticky=W)
         ttk.Button(mainframe, text="Greater Medium", command=lambda: self.choose_type_max("Greater Medium")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 4, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 4, sticky=W)
         ttk.Button(mainframe, text="Lesser Major", command=lambda: self.choose_type_max("Lesser Major")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 5, sticky=W)
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 5, sticky=W)
         ttk.Button(mainframe, text="Greater Major", command=lambda: self.choose_type_max("Greater Major")).grid(
-            column=BASE_POS_MAX_ITEM_COL, row=STARTPOS_ROW_MIN_TYPE_ROW + 6, sticky=W)
-        
+            column=BASE_POS_MAX_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 6, sticky=W)
+
+        ttk.Label(mainframe, text="Item Type").grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW - 1, sticky=W)
+        ttk.Button(mainframe, text="All ON", command=lambda: self.choose_all_types()).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW, sticky=W)
+        ttk.Button(mainframe, text="All OFF", command=lambda: self.all_types_off()).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 1, sticky=W)
+        ttk.Checkbutton(
+            mainframe,
+            text="Belts",
+            variable=self.belt
+            ).grid(
+            column=BASE_POS_TYPE_ITEM_COL,
+            row=START_POS_ROW_MIN_TYPE_ROW + 2,
+            sticky=W)
+        ttk.Checkbutton(mainframe, text="Body", variable=self.body).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 3, sticky=W)
+        ttk.Checkbutton(mainframe, text="Chest", variable=self.chest).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 4, sticky=W)
+        ttk.Checkbutton(mainframe, text="Eyes", variable=self.eyes).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 5, sticky=W)
+        ttk.Checkbutton(mainframe, text="Feet", variable=self.feet).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 6, sticky=W)
+        ttk.Checkbutton(mainframe, text="Hands", variable=self.hands).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 7, sticky=W)
+        ttk.Checkbutton(mainframe, text="Head", variable=self.head).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 8, sticky=W)
+        ttk.Checkbutton(mainframe, text="Headband", variable=self.headband).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 9, sticky=W)
+        ttk.Checkbutton(mainframe, text="Neck", variable=self.neck).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 10, sticky=W)
+        ttk.Checkbutton(mainframe, text="Shoulders", variable=self.shoulders).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 11, sticky=W)
+        ttk.Checkbutton(mainframe, text="Wrists", variable=self.wrists).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 12, sticky=W)
+        ttk.Checkbutton(mainframe, text="Slot-less", variable=self.slot_less).grid(
+            column=BASE_POS_TYPE_ITEM_COL, row=START_POS_ROW_MIN_TYPE_ROW + 13, sticky=W)
+
+
+        #ttk.Checkbutton()
+        #    .Label(mainframe, textvariable=self.belt.get()).grid(
+         #   column=BASE_POS_TYPE_ITEM_COL+1, row=START_POS_ROW_MIN_TYPE_ROW+2, sticky=W)
+        ttk.Label(mainframe, text="%self.belt").grid(column=BASE_POS_TYPE_ITEM_COL+1, row=START_POS_ROW_MIN_TYPE_ROW+3, sticky=W)
         ttk.Label(mainframe, text="number of dice").grid(column=3, row=1, sticky=W)
         ttk.Label(mainframe, text="roll: ").grid(column=1, row=2, sticky=E)
         ttk.Label(mainframe, text="total").grid(column=3, row=2, sticky=W)
@@ -93,6 +159,34 @@ class DiceDisplay:
 
     def choose_type_max(self, item_maximum):
         self.item_max.set(item_maximum)
+
+    def choose_all_types(self):
+        self.belt.set(True)
+        self.body.set(True)
+        self.chest.set(True)
+        self.eyes.set(True)
+        self.feet.set(True)
+        self.hands.set(True)
+        self.head.set(True)
+        self.headband.set(True)
+        self.neck.set(True)
+        self.shoulders.set(True)
+        self.wrists.set(True)
+        self.slot_less.set(True)
+
+    def all_types_off(self):
+        self.belt.set(False)
+        self.body.set(False)
+        self.chest.set(False)
+        self.eyes.set(False)
+        self.feet.set(False)
+        self.hands.set(False)
+        self.head.set(False)
+        self.headband.set(False)
+        self.neck.set(False)
+        self.shoulders.set(False)
+        self.wrists.set(False)
+        self.slot_less.set(False)
 
 
 root = Tk()
